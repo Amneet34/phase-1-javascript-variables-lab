e0e313f6711f823af5a9c535c7818bea6d173ae4
Amneet Singh <+>
2022-11-02 10:02:44 -0400

    commit tree a0fe1a37ca30767dd576d8a3759867937507393b

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+const companyName = ('Scuber'); {
+    let companyName = "Scuber"
+}
+
+const mostProfitableNeighborhood = ('Chelsea'); {
+    let mostProfitableNeighborhood = "Chelsea"
+}
+
+const companyCeo = ('Susan Smith'); {
+    let companyCeo = "Susan Smith"
+}
+
